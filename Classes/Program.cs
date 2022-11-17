55b525697a4f41f705c56eb650fcb32789ba0fb3
Andrew Edmonds <edmondsandrew9@gmail.com>
2022-11-16 23:39:01 -0500

    initial commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          Car newCar = new Car();
+            newCar.Year = 2005;
+            newCar.Make = "Honda";
+            newCar.Model = "Civic";
+            Console.WriteLine($"Your car is a {newCar.Year} {newCar.Make} {newCar.Model}!");
+
         }
     }
 }
